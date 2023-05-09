5b52c5cdb2e4d0d4943cc06a96b0d5ed43b6f1b6
BetoBullicio8025 <126055898+BetoBullicio8025@users.noreply.github.com>
2023-05-09 14:29:05 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //Ventana ventana1 = new Ventana("Layout");
+        VentanaGrid ventana2 = new VentanaGrid("Layout Cuadricula");
+
+    }
+}
